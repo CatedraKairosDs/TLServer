c9214877bd71abbdccf903c05f307ed89ef3feec
Ubuntu <ubuntu@ip-172-31-33-141.eu-west-1.compute.internal>
2017-12-19 15:58:59 +0000

    Current AWS

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -11,7 +11,7 @@ const Companies = require('../models/companies')
 
 //guardado de perfil
 function saveProfiles (req, res){
-    
+    console.log(req.body.puesto)    
     let profile = new Profiles()
 
     profile.puesto = req.body.puesto
